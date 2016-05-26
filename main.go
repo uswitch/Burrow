0967cf7cbcb2414057c66672aad30bfe9c672a4e
Ragnar Dahlén <r.dahlen@gmail.com>
2016-05-26 10:35:54 +0100

    Don't redirect stderr/stdout or attempt to manage logfiles.

--- main.go
+++ main.go
@@ -120,9 +120,7 @@ func burrowMain() int {
 	createPidFile(appContext.Config.General.LogDir + "/" + appContext.Config.General.PIDFile)
 	defer removePidFile(appContext.Config.General.LogDir + "/" + appContext.Config.General.PIDFile)
 
-	// Set up stderr/stdout to go to a separate log file
-	openOutLog(appContext.Config.General.LogDir + "/burrow.out")
-	fmt.Println("Started Burrow at", time.Now().Format("January 2, 2006 at 3:04pm (MST)"))
+	log.Infof("Started Burrow at %s", time.Now().Format("January 2, 2006 at 3:04pm (MST)"))
 
 	// If a logging config is specified, replace the existing loggers
 	if appContext.Config.General.LogConfig != "" {
